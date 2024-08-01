d2cf9755e1e4a8cb171fa8361eb799b3e504783b
sebas957 <baxtian957@hotmail.com>
2024-08-01 16:18:12 -0500

    ajustes

--- public/js/general.js
+++ public/js/general.js
@@ -3,3 +3,16 @@ function abrirVentana(url, Nombre, Alto, Ancho) {
     var randomnumber = Math.floor((Math.random()*100)+1);
     window.open(url,Nombre + "-" +randomnumber, 'width=' + Ancho + ', height=' + Alto +',scrollbars=1,menubar=0,resizable=1');
 }
+
+function ChequearTodosTabla(source, nombre) {
+    // Obtener todos los checkboxes con el nombre dado
+    const checkboxes = document.querySelectorAll(`input[name="${nombre}"]`);
+    // Determinar si todos los checkboxes están marcados
+    const allChecked = Array.from(checkboxes).every(checkbox => checkbox.checked);
+
+    // Si todos están marcados, desmarcar todos, de lo contrario, marcar todos
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = !allChecked ? source.checked : false;
+    });
+}
+
